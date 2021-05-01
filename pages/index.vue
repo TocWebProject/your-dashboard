0597e7891918ff38367f48e343a858f51510c1bf
Toc <tocwebtoc@gmail.com>
2021-05-01 13:44:22 +0200

    Playing with Gsap, staggering multiple div

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="min-w-full  flex flex-col">
       <h1 
-        class="text-5xl text-blue-900 mt-20"
+        class="text-5xl text-blue-900 m-20"
         v-gsap.fromTo="[
         { opacity: 0, x: -100 },
         { opacity: 1, x: 0 }
@@ -11,10 +11,10 @@
         Your Dashboard
       </h1>
       <NuxtLink to="/sport">Sport</NuxtLink>
-      <div class="h-80 bg-blue-300"></div>
-      <div class="h-80 bg-blue-400"></div>
-      <div class="h-80 bg-blue-500"></div>
-      <div class="scroll-animate h-80 bg-blue-600">
+      <div class="scroll-animate h-96 bg-blue-300"></div>
+      <div class="scroll-animate h-96 bg-blue-400"></div>
+      <div class="scroll-animate h-96 bg-blue-500"></div>
+      <div class="scroll-animate h-96 bg-blue-600">
       </div>
     </div>
   </div>
@@ -33,15 +33,30 @@ export default {
     animateOnScroll() {
       // this.$gsap.to(window, { duration: 2, scrollTo: 1000 })
 
-      this.$gsap.from('.scroll-animate', {
-        ease: 'Power1.easeInOut',
-        scrollTrigger: {
-          trigger: '.scroll-animate',
-          scrub: 2,
-          start: 'top bottom',
-          end: 'top 60%'
-          },
-          opacity: 0,
+      // this.$gsap.from('.scroll-animate', {
+      //   ease: 'Power1.easeInOut',
+      //   scrollTrigger: {
+      //     trigger: '.scroll-animate',
+      //     scrub: 2,
+      //     start: 'top bottom',
+      //     end: 'top 60%'
+      //     },
+      //     opacity: 0,
+      // })
+
+      const gsap = this.$gsap
+      let boxes = document.querySelectorAll('.scroll-animate');
+      boxes.forEach( function( elem ) {
+          gsap.from(elem, {
+              scrollTrigger: {
+                  trigger: elem,
+                  start: 'top 60%',
+                  end: 'top 80%',
+                  toggleActions: "play none reverse none"
+              },
+              rotation: 180,
+              duration: 2
+          })
       })
     }
   }
