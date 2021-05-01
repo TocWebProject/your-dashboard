2fa19ee0e09b4e5e6ffefe29dfc10484f9e8902a
Toc <tocwebtoc@gmail.com>
2021-05-01 14:15:16 +0200

    Clean index page, moving gsap tricks to sport page

--- pages/index.vue
+++ pages/index.vue
@@ -10,11 +10,7 @@
       >
         Your Dashboard
       </h1>
-      <NuxtLink to="/sport">Sport</NuxtLink>
-      <div class="scroll-animate h-96 bg-blue-300"></div>
-      <div class="scroll-animate h-96 bg-blue-400"></div>
-      <div class="scroll-animate h-96 bg-blue-500"></div>
-      <div class="scroll-animate h-96 bg-blue-600">
+      <NuxtLink class="text-2xl font-semibold text-blue-800 hover:text-blue-600" to="/sport">Sport</NuxtLink>
       </div>
     </div>
   </div>
@@ -24,42 +20,6 @@
 export default {
   name: 'dashboard',
   transitions: 'page',
-
-  mounted() {
-    this.animateOnScroll()
-  },
-
-  methods: {
-    animateOnScroll() {
-      // this.$gsap.to(window, { duration: 2, scrollTo: 1000 })
-
-      // this.$gsap.from('.scroll-animate', {
-      //   ease: 'Power1.easeInOut',
-      //   scrollTrigger: {
-      //     trigger: '.scroll-animate',
-      //     scrub: 2,
-      //     start: 'top bottom',
-      //     end: 'top 60%'
-      //     },
-      //     opacity: 0,
-      // })
-
-      const gsap = this.$gsap
-      let boxes = document.querySelectorAll('.scroll-animate');
-      boxes.forEach( function( elem ) {
-          gsap.from(elem, {
-              scrollTrigger: {
-                  trigger: elem,
-                  start: 'top 60%',
-                  end: 'top 80%',
-                  toggleActions: "play none reverse none"
-              },
-              rotation: 180,
-              duration: 2
-          })
-      })
-    }
-  }
-}
+}  
 </script>
 
