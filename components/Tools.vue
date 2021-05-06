f8e3d01ca8812d425cf63a90bab8445d16cd5ff4
Toc <tocwebtoc@gmail.com>
2021-05-06 18:49:59 +0200

    Fixed <a> aria-label in Tools.vue

--- components/Tools.vue
+++ components/Tools.vue
@@ -8,6 +8,7 @@
                 <div class="lg:hidden">
                     <NuxtLink 
                         to="/"
+                        aria-label="Back home"
                         class="flex justify-center items-center w-14 h-14 rounded-xl bg-gray-50 mb-2">
                         <svg class="w-8 h-8 text-blue-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>   
                     </NuxtLink>
